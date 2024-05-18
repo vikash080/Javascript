da7720a774b9db78bdd0b6953b7ff126dba195c8
vikash080 <vikash08011999@gmail.com>
2024-05-18 22:26:02 +0530

    Js Basics

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,55 @@
+let score = null
+
+//console.log(typeof score)
+//console.log(typeof(score))
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber)
+
+// "33" => 33
+// "33abc" => NaN
+//true => 1; false => 0
+
+let isLoggedIn = ""
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+//"" => false
+// "vikash" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+//console.log(stringNumber);
+//console.log(typeof (stringNumber))
+
+// *******************OPERATIONS**********************
+
+let value = 3
+let negValue = -value
+//console.log(negValue)
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1+str2
+//console.log(str3)
+
+//console.log("1"+ 2);
+//console.log(1+ "2");
+//console.log("1" + "2");
+//onsole.log("1" + 2 + 2);
+//console.log(1 + 2 +"2")
+
+//console.log(+true);
+//console.log(+"");
+
+let num1, num2, num3 
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+console.log(2 > 1);
+console.log(2 >= 1);
+console.log(2== 1);
+console.log(2 != 1);
+
+
+//  THIS CONVERSIONS WILL ALWAYS CONFUSE US
+console.log("2" > 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined >= 0);
+
+
+// === it checks value as well as the datatype of the variable or element
+console.log("2" === 2);
+
