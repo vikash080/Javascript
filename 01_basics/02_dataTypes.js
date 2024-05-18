692cce9c61509be1cbb07ddfd7db94f7e904bb92
vikash080 <vikash08011999@gmail.com>
2024-05-18 15:18:59 +0530

    Js basics

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict";// treate all JS code as newer version
+
+//  alert( 3 + 3)  // we are using nodejs, not browser
+
+console.log(3 + 
+    3) // code readability should be high
+
+console.log("Hello")
+
+let name ="Vikash" // String
+let age = 12 // integer
+let isLoggedIn = false // boolean
+
+//number => 2 to power 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => a special data type used when there is no value
+//undefined => variable has been declared but it's currently holding no value
+//symbol => unique
+
+
+//object => {}
+
+
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
