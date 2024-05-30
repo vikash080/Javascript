d3f5cb878213ce0e86bdc6d294eab5f8c456f795
vikash080 <vikash08011999@gmail.com>
2024-05-31 00:58:33 +0530

    Js intermediate

---
+++ 05_iterations.js/five.js
@@ -0,0 +1,38 @@
+const coding = [ "js" , "rb" , "java" , "python" ,"cpp"]
+
+// coding.forEach( function (item) {
+//     console.log(item);
+// } )
+
+// coding.forEach( (item) => {
+//     console.log(item);
+// } )
+
+// function printMe(item) {
+//      console.log(item);
+// }
+
+// coding.forEach(printMe)
+
+// coding.forEach( (item , index , arr) => {
+//     console.log(item, index, arr);
+// })
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "Python",
+        languageFileName : "py"
+    }
+]
+
+myCoding.forEach((intem) => {
+    console.log(intem.languageName, intem.languageFileName);
+})
--- 05_iterations.js/four.js
+++ 05_iterations.js/four.js
@@ -1,10 +1,26 @@
 const myObject = {
-    js : "javascript",
-    cpp : "C++",
-    rb : "Ruby",
-    swipt : "Swift by apple"
+    js: "javascript",
+    cpp: "C++",
+    rb: "Ruby",
+    swipt: "Swift by apple"
 }
 
 for (const key in myObject) {
-    console.log(`${key} shortcut is for ${myObject[key]}`);
+    //console.log(`${key} shortcut is for ${myObject[key]}`);
 }
+
+const program = ["js", "rb", "py", "java", "cpp"]
+
+for (const key in program) {
+    //console.log(program[key]);
+}
+
+// Map is not at all irrateable
+// const map = new Map()
+// map.set('In' , "India")
+// map.set('USA' , "United States of America")
+// map.set('UK' , "United Kingdom")
+
+// for (const key in map) {
+//     console.log(key);
+// }
